0dc9771ffd78d4f8d0ecb131555d12165d652a3f
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2025-03-11 20:21:31 +0100

    Render 16 divs with js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("hello Etch-a-Sketch");
+
+function createGridOfDivs() {
+  const divs = 16;
+  const height = "120px";
+  const width = "120px";
+  const container = document.querySelector(".container");
+
+  for (let i = 0; i < divs; i++) {
+    const div = document.createElement("div");
+    div.style.height = height;
+    div.style.width = width;
+    div.style.backgroundColor = "black";
+    div.id = i;
+    div.className = "grid";
+
+    container.appendChild(div);
+  }
+}
+
+createGridOfDivs();
